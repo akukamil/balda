16115b3442c759da0ebdae8ef30e6ffbb345c7a1
akukamil <chamilioner_kid@mail.ru>
2022-01-06 11:23:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3130,8 +3130,8 @@ async function load_resources() {
 	return;*/
 
 
-	//let git_src="https://akukamil.github.io/chess/"
-	let git_src=""
+	let git_src="https://akukamil.github.io/balda/"
+	//let git_src=""
 
 
 	game_res=new PIXI.Loader();
