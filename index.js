61c768c0811b1d310b3157c8b5d6d43edf50fbf2
akukamil <chamilioner_kid@mail.ru>
2022-01-10 10:41:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3081,8 +3081,8 @@ async function load_resources() {
 	return;*/
 
 
-	//let git_src="https://akukamil.github.io/balda/"
-	let git_src=""
+	let git_src="https://akukamil.github.io/balda/"
+	//let git_src=""
 
 
 	game_res=new PIXI.Loader();
