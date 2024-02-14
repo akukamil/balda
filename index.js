229504a9d1bfbcc2ed25c1a479343b1bab78e0c8
akukamil <chamilioner_kid@mail.ru>
2024-02-14 12:06:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4139,7 +4139,7 @@ async function init_game_env() {
 	fbs.ref("inbox/"+my_data.uid).on('value', (snapshot) => { process_new_message(snapshot.val());});
 
 	//обновляем данные в файербейс так как могли поменяться имя или фото
-	fbs.ref("players/"+my_data.uid).set({name:my_data.name, pic_url: my_data.pic_url, rating : my_data.rating, games : my_data.games, tm:firebase.database.ServerValue.TIMESTAMP});
+	fbs.ref("players/"+my_data.uid).set({name:my_data.name, pic_url: my_data.pic_url, avatar_tm:my_data.avatar_tm, nick_tm:my_data.nick_tm, rating : my_data.rating, games : my_data.games, tm:firebase.database.ServerValue.TIMESTAMP});
 
 	//устанавливаем мой статус в онлайн
 	set_state({state : 'o'});
